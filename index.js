02d5f1ce7abd5c1c3e01e60b27d30943c596c963
Kirill Romanov <djaler1@gmail.com>
2021-03-25 15:31:07 +0300

    feat: extend preset from conventional-changelog-conventionalcommits

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 'use strict'
-const Q = require('q')
-const conventionalChangelog = require('./conventional-changelog')
-const parserOpts = require('./parser-opts')
-const recommendedBumpOpts = require('./conventional-recommended-bump')
-const writerOpts = require('./writer-opts')
+const config = require('conventional-changelog-conventionalcommits')
 
-module.exports = Q.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts])
-  .spread((conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => {
-    return { conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts }
-  })
+module.exports = config({
+    types: [
+        { type: 'feat', section: 'Features' },
+        { type: 'fix', section: 'Bug Fixes' },
+        { type: 'improvement', section: 'Improvements' },
+        { type: 'perf', section: 'Performance Improvements' },
+        { type: 'deps', section: 'Dependencies' },
+        { type: 'docs', section: 'Documentation', hidden: true },
+        { type: 'style', section: 'Styles', hidden: true },
+        { type: 'refactor', section: 'Code Refactoring', hidden: true },
+        { type: 'test', section: 'Tests', hidden: true },
+        { type: 'build', section: 'Build System', hidden: true },
+        { type: 'ci', section: 'Continuous Integration', hidden: true },
+    ]
+})
